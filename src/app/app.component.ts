f4b7636b74b8ca4d90dc42922b260290bc0b5d78
Emiel de Jong <emieldejong@xs4all.nl>
2022-09-22 08:59:45 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   movies = ['the game', 'big fish', 'titanic', 'The Wizard of Oz'];
